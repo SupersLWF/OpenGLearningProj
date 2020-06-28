a2205d39b6027c630669c304a3069405ad2bc377
SupersLWF <jianghuaiyue@outlook.com>
2020-06-28 16:58:09 +0800

    change main.cpp to add mesh class

--- GLearning_1/main.cpp
+++ GLearning_1/main.cpp
@@ -1,18 +1,29 @@
 #include<iostream>
 #include<GL/glew.h>
 #include"Display.h"
-#include "main.h"
+#include"Mesh.h"
 
 int main(int _Argc, char** argv)
 {
-	std::cout << "test program" << std::endl;
+	//std::cout << "test program" << std::endl;
 	Display display(800, 400, "testprogram");
 
+	//顶点数组
+	Vertex vertices[] = {Vertex(glm::vec3(-0.5,-0.5,0)),
+						 Vertex(glm::vec3(0,0.5,0)),
+						 Vertex(glm::vec3(0.5,-0.5,0))};
+
+	//初始化片元网格
+	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
 
 	//此while循环用于接收opengl调用
 	while (!display.IsClosed())
 	{
 		display.Clear(0.0F/*RED*/, 0.15F/*GREEN*/, 0.3F/*BLUE*/, 1.0F/*ALHFA*/);
+		
+		//绘制片元网格
+		mesh.Draw();
+		
 		display.update();//display内部有缓冲区交换函数，此函数作用是交换，然后update窗口
 
 	}
